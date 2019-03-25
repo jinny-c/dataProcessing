66387272b64343c0f6215201fb2f1c9677375526
jidd <chaijindong>
2019-03-25 10:28:38 +0800

    for update

--- src/main/java/com/data/com/data/demo/FileDateFormat.java
+++ src/main/java/com/data/com/data/demo/FileDateFormat.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.data.com.data.demo;
 
 import java.text.DecimalFormat;
@@ -32,7 +15,7 @@ public final class FileDateFormat {
 	
   public static void main(String[] args) throws Exception {
 	  doSpark();
-      testNumber();
+      //testNumber();
   }
     private static void testNumber(){
         DecimalFormat df=new DecimalFormat(".0000");
@@ -43,6 +26,7 @@ public final class FileDateFormat {
   private static void doSpark(){
 	    String path = "file:/D:/workspace/lakala/POSP/logs/log2/";
 	    path = "file:/C:\\Users\\jidd\\Desktop/abc.txt";
+	    path = "file:/C:\\Users\\jidd\\Desktop/text.txt";
 	    SparkSession spark = SparkSession
 	      .builder().master("local[4]")
 	      .appName("file date formate")
